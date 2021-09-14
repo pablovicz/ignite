69b39c5bca4713bef28a68082b3a400623cb2ac3
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2021-09-14 20:51:14 -0300

    Chapter I - Configurando ambiente - (2/2)

--- reactjs/01-github-explorer/src/App.jsx
+++ reactjs/01-github-explorer/src/App.jsx
@@ -1,3 +1,8 @@
+import './styles/global.scss';
+
+
 export function App(){
+    //throw new Error('Deu ruim tio');
+
     return <h1>Hello React World</h1>
 }
--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const IsDevelopment = process.env.NODE_ENV !== 'production';
+
+
 module.exports = {
-    mode: 'development',
+    mode: IsDevelopment ? 'development' : 'production',
+    devtool:IsDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -26,6 +30,11 @@ module.exports = {
                 exclude: /node_modules/,
                 use: 'babel-loader',
 
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'], 
             }
         ]
     }
