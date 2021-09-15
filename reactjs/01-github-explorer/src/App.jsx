e09c22b62143b0f698f8fb4d74db113c63e4f6ca
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2021-09-15 08:27:24 -0300

    Chapter I - Conceitos Importantes - Propriedades do React

--- reactjs/01-github-explorer/src/App.jsx
+++ reactjs/01-github-explorer/src/App.jsx
@@ -1,8 +1,12 @@
-import './styles/global.scss';
+import { Counter } from "./components/Counter";
+import { RepositoryList } from "./components/RepositoryList";
+import "./styles/global.scss";
 
-
-export function App(){
-    //throw new Error('Deu ruim tio');
-
-    return <h1>Hello React World</h1>
+export function App() {
+  return (
+    <div>
+      <RepositoryList />
+      <Counter />
+    </div>
+  );
 }
