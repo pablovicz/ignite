9db6e8e264db838f9e35277acdc896b43d221e29
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2021-09-20 21:31:03 -0300

    Chapter II - Estrutura da Aplicação

---
+++ reactjs/dt-money/src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+
+
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello React World</h1>
+      <GlobalStyle />
+    </div>
+  );
+}
+
