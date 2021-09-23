520eaacd0a7f80936fd2e3de0a61b297fe418449
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2021-09-23 09:18:03 -0300

    Chapter II - Modal & Forms - Done

--- reactjs/dt-money/src/components/NewTransactionModal/index.tsx
+++ reactjs/dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,16 +1,36 @@
 import ReactModal from "react-modal";
-import { Container } from "./styles";
+import { Container, TransacionTypeContainer, RadioBox } from "./styles";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
+import { FormEvent, useState } from "react";
+import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
-export function NewTransactionModal({
-  isOpen,
-  onRequestClose,
-}: NewTransactionModalProps) {
+
+
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
+
+
+  const [type, setType] = useState("deposit");
+
+  function handleCreateNewTransaction(event: FormEvent){
+    event.preventDefault();
+
+    const data = {title, value, category, type}
+
+    api.post('/transactions', data);
+  }
+
   return (
     <ReactModal
       isOpen={isOpen}
@@ -18,19 +38,51 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-      <button 
-        type="button" 
+      <button
+        type="button"
         onClick={onRequestClose}
         className="react-modal-close"
-    >
+      >
         <img src={closeImg} alt="Fechar modal" />
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar Transação</h2>
-        <input placeholder="Título" />
-        <input type="number" placeholder="Valor" />
-
-        <input placeholder="Categoria" />
+        <input 
+          placeholder="Título" 
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
+        <input 
+          type="number" 
+          placeholder="Valor" 
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
+        />
+        <TransacionTypeContainer>
+          <RadioBox 
+            type="button" 
+            onClick={() => setType("deposit")}
+            isActive={type === 'deposit'}    // propriedade custom criada com qualquer nome que se queira
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+          <RadioBox 
+            type="button" 
+            onClick={() => setType("withdraw")}
+            isActive={type === 'withdraw'}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+        </TransacionTypeContainer>
+        <input 
+          placeholder="Categoria" 
+          value={category}
+          onChange={event => setCategory(event.target.value)}
+        />
         <button type="submit">Cadastrar</button>
       </Container>
     </ReactModal>
