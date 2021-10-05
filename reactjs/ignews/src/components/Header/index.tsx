6ac3d7d6890f517bf714104868911b6e2303ebcf
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2021-10-05 09:19:03 -0300

    Chapter III - Frontend JAMStack - Trabalhando com CMS - Consumindo PrismicAPI, React cloneElement

--- reactjs/ignews/src/components/Header/index.tsx
+++ reactjs/ignews/src/components/Header/index.tsx
@@ -1,15 +1,26 @@
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 
 export function Header() {
+
+    const { asPath } = useRouter();
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active} href="">Home</a>
-                    <a href="">Posts</a>
+                    <ActiveLink href="/" activeClassName={styles.active}>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink href="/posts" activeClassName={styles.active} prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 <SignInButton />
             </div>
