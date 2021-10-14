034df97523b6b291ccba479df8bf045fd2d5dc6d
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2021-10-14 09:11:21 -0300

    Chapter IV - Interfaces com ChakraUI - Pagina Signin, Dashboard

--- reactjs/dashgo/src/pages/index.tsx
+++ reactjs/dashgo/src/pages/index.tsx
@@ -1,6 +1,33 @@
+import { Flex,Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
+
 
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"  // medida do chakra -> 8 = 2rem = 32px
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <Input type="email" label="E-mail" name="email"/>
+          <Input type="password" label="Senha" name="password"/>
+    
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
